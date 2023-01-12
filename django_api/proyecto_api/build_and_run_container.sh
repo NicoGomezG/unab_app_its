b3458115e72743eaab6b340171bd86b512e4b6c3
Nicolas Gomez <n.gomezgodoy@uandresbello.edu>
2023-01-12 19:04:00 -0300

    Actualizacion de repo

--- django_api/proyecto_api/build_and_run_container.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker build --no-cache -t contenedor .
-docker run -d -p 80:80 contenedor
--- django_api/proyecto_api/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python manage.py makemigrations
-python manage.py migrate --no-input
-python manage.py collectstatic --no-input
-service apache2 restart
-gunicorn proyecto_api.wsgi:application
